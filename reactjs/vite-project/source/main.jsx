eacc636464babc2bb3a42d3eaf2f6f32c7c70456
Life-In-Rio <abyan0417@abudzar.sch.id>
2025-01-27 11:42:57 +0700

    Tugas 15

--- reactjs/vite-project/source/main.jsx
+++ reactjs/vite-project/source/main.jsx
@@ -7,10 +7,12 @@ import AxiosStore from "./Tugas14/soal-soal/soalgambar22";
 import Apatuh from "./Tugas14/soal-soal/soalgambar12";
 import Axioshop from "./Tugas14/soal-soal/soalgambar2";
 
-
+import SimpleUserForm from "./Tugas15/Tes soal/tessoal";
+import InputForm from "./Tugas15/soal/soal 1/soalgambar1";
+import Crudd from "./Tugas15/soal/soal 2/soalgambar1";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Axioshop />
+    <Crudd />
   </StrictMode>
 );
