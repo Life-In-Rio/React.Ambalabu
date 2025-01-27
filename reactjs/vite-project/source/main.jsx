e0df2fe07a270d697dba5203d363beb9f99522f0
Life-In-Rio <abyan0417@abudzar.sch.id>
2025-01-27 09:50:03 +0700

    Tugas 14

--- reactjs/vite-project/source/main.jsx
+++ reactjs/vite-project/source/main.jsx
@@ -1,22 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./input.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Nigga from "./Tugas11/Soal/soalgamabr12";
-import Negro from "./Tugas11/Soal/soalgambar22";
-import { HomePage } from "./Tugas12/soal-soal/soal 1/soalgamabr1";
-import CobaRouter from "./Tugas12/Tes Soal/tessoal";
-import { NotFoundPage } from "./Tugas12/soal-soal/soal 2/soalgambar2";
-import Hytam from "./Tugas12/soal-soal/soal 3/soalgambar3";
-import Bozzo from "./Tugas12/soal-soal/soal 4/soalgambar4";
-import Banger from "./Tugas12/soal-soal/soal 5/soalgambar5";
-import CobaAxios from "./Tugas13/tessoal";
-import Axios from "./Tugas13/soalgambar1";
-import ProductList from "./Tugas9/soal-soal/soalgambar1,2,3";
+
+import BookList from "./Tugas14/soal-soal/soalgambar12";
+import AxiosStore from "./Tugas14/soal-soal/soalgambar22";
+import Apatuh from "./Tugas14/soal-soal/soalgambar12";
+import Axioshop from "./Tugas14/soal-soal/soalgambar2";
+
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ProductList/>
+    <Axioshop />
   </StrictMode>
 );
