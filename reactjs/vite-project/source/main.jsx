812eeca7d9985512ea40b44d9fdd111d8b24f35a
Life-In-Rio <abyan0417@abudzar.sch.id>
2025-01-28 13:25:48 +0700

    Tugas 16

--- reactjs/vite-project/source/main.jsx
+++ reactjs/vite-project/source/main.jsx
@@ -1,18 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./input.css";
+import App from "./Tugas16-jwt/soalgambar1";
 
-import BookList from "./Tugas14/soal-soal/soalgambar12";
-import AxiosStore from "./Tugas14/soal-soal/soalgambar22";
-import Apatuh from "./Tugas14/soal-soal/soalgambar12";
-import Axioshop from "./Tugas14/soal-soal/soalgambar2";
-
-import SimpleUserForm from "./Tugas15/Tes soal/tessoal";
-import InputForm from "./Tugas15/soal/soal 1/soalgambar1";
-import Crudd from "./Tugas15/soal/soal 2/soalgambar1";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Crudd />
+    <App/>
   </StrictMode>
 );
