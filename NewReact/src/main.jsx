0a57554a545d4e2b59c3a5b46ae8eb397a54accc
Life-In-Rio <abyan0417@abudzar.sch.id>
2025-02-10 11:35:13 +0700

    tugas 19

--- NewReact/src/main.jsx
+++ NewReact/src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 
 import AxiosShop from "./Tugas14-Dynamic router/soal-soal/soal 2/soalgambar23";
 import Plate2 from "./Tugas18-CRUD(2)/soalgambar2";
+import Counter from "./Tugas19-Zustand/tes soal/tessoal";
+import CustomerCounter from "./Tugas19-Zustand/soal-soal/soalgambar1";
+import TodoApp from "./Tugas19-Zustand/soal-soal/soalgambar2";
+
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Plate2 />
+    <TodoApp/>
   </StrictMode>
 );
