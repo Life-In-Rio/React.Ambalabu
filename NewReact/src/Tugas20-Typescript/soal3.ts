2bee996dac7af4125999f1a7316421db19b15bd7
Life-In-Rio <abyan0417@abudzar.sch.id>
2025-02-12 10:54:04 +0700

    Tugas 20

---
+++ NewReact/src/Tugas20-Typescript/soal3.ts
@@ -0,0 +1,13 @@
+interface Mahasiswa {
+    nama: string;
+    umur: number;
+    jurusan: string;
+}
+
+const mhs1: Mahasiswa = {
+    nama: "Abyan",
+    umur: 20,
+    jurusan: "Bahasa"
+};
+
+console.log(mhs1);
